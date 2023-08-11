3885ab8be01239e4a64bc97075c61bb83c631013
teageto <turnablees@gmail.com>
2023-08-11 14:30:25 -0300

    Se cambian las variables a la function validation

--- js/validacion.js
+++ js/validacion.js
@@ -6,14 +6,16 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-let name = document.getElementById('nombre').value;
-let sname = document.getElementById('apellido').value;
-let email = document.getElementById('email').value;
-let pass1 = document.getElementById('password1').value;
-let pass2 = document.getElementById('password2').value;
-let passValidation = pass1 === pass2;
 
 function validation() {
+    
+    let name = document.getElementById('nombre').value;
+    let sname = document.getElementById('apellido').value;
+    let email = document.getElementById('email').value;
+    let pass1 = document.getElementById('password1').value;
+    let pass2 = document.getElementById('password2').value;
+    let passValidation = pass1 === pass2;
+
     if ( name != '' && sname != '' && email != '' && email.includes('@') && passValidation) {
         showAlertSuccess();
     } else {
