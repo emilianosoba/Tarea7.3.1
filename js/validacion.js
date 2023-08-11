6cf2d097fbc36a5da71e6f456b29b1e48ed3874c
FavriDev98 <favripn@gmail.com>
2023-08-11 13:34:46 -0300

    alertas y validacion

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let name = document.getElementById('nombre').value;
+let sname = document.getElementById('apellido').value;
+let email = document.getElementById('email').value;
+let passValidation;
+
+if (getElementById('password1') == getElementById('password2')) {
+    passValidation = true;
+}
+
+if ( name != '' && sname != '' && email != '' && email.includes('@') && passValidation) {
+    showAlertSuccess();
+} else {
+    showAlertError();
+}
